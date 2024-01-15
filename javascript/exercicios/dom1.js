acf9e9d24941d6ebd59f6f38e29f74a263603a18
Sarah Prando <sarahprando40@gmail.com>
2024-01-15 17:45:53 -0300

    ex js dom

---
+++ javascript/exercicios/dom1.js
@@ -0,0 +1,19 @@
+// Retorne o url da página atual utilizando o objeto window
+console.log(window.location.href);
+
+// Seleciona o primeiro elemento da página que
+// possua a classe ativo
+
+/*
+const teste = document.querySelector('h1');
+
+teste.classList
+
+*/
+
+// Retorne a linguagem do navegador
+
+console.log(navigator.language)
+
+// Retorne a largura da janela 
+console.log(window.innerWidth);
