6d72ef88c992f50292bd8f55dde7ebaf6a285ce5
Sarah Prando <sarahprando40@gmail.com>
2024-01-17 15:23:03 -0300

    js animais fantasticos

--- animais-fantasticos/script.js
+++ animais-fantasticos/script.js
@@ -1,43 +1,88 @@
-// Verifique a distância da primeira imagem
-// em relação ao topo da página
-const primeiraImg = document.querySelector('img');
+function initTabNav() {
+const tabMenu = document.querySelectorAll('.js-tabmenu li');
+const tabContent = document.querySelectorAll('.js-tabcontent section');
 
-console.log(primeiraImg.offsetTop);
+if(tabMenu.length && tabContent.length) {
+tabContent[0].classList.add('ativo');
 
-// Retorne a soma da largura de todas as imagens
-function somaImagens() {
-const imagens = document.querySelectorAll('img');
-let soma = 0;
-imagens.forEach((imagem) => {
-    soma += imagem.offsetWidth;
-});
-console.log(soma);
+function activeTab(index){
+    tabContent.forEach((section) => {
+        section.classList.remove('ativo');
+    });
+    tabContent[index].classList.add('ativo');
 }
 
-window.onload = function(){
-    somaImagens();
-}
-// Verifique se os links da página possuem
-// o mínimo recomendado para telas utilizadas
-// com o dedo. (48px/48px de acordo com o google)
-const links = document.querySelectorAll('a');
-
-links.forEach((link) => {
-    const linkWidth = link.offsetWidth;
-    const linkHeight = link.offsetHeight;
-
-    if(linkWidth >= 48 && linkHeight >= 48) {
-        console.log('Possui acessibilidade');
-    } else {
-        console.log('Não possui boa acessibilidade')
-    }
+tabMenu.forEach((itemMenu, index) => {
+    itemMenu.addEventListener('click', () => {
+        activeTab(index);
+    })
 })
-
-// Se o browser for menor que 720px,
-// adicione a classe menu-mobile ao menu
-const browserSmall = window.matchMedia('(max-width: 720px)').matches;
-
-if(browserSmall) {
-    const menu = document.querySelector('.menu');
-    menu.classList.add('menu-mobile');
 }
+}
+
+initTabNav();
+
+function initAccordion() {
+const accordionList = document.querySelectorAll('.js-accordion dt');
+const activeClass = 'ativo'
+
+if(accordionList.length){
+accordionList[0].nextElementSibling.classList.add(activeClass);
+
+function activeAccordion(event) {
+    this.classList.toggle(activeClass);
+    this.nextElementSibling.classList.toggle(activeClass);
+}
+
+accordionList.forEach((item) => {
+    item.addEventListener('click', activeAccordion);
+});
+}
+}
+
+initAccordion();
+
+function initScrollSuave() {
+const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute('href');
+    const section = document.querySelector(href);
+   
+    section.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+    })
+}
+
+linksInternos.forEach((link) => {
+    link.addEventListener('click', scrollToSection);
+})
+}
+
+initScrollSuave();
+
+
+function initAnimacaoScroll(){
+const sections = document.querySelectorAll('.js-scroll');
+
+if(sections.length){
+const windowMetade = window.innerHeight * 0.6;
+
+function animaScroll() {
+    sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top - windowMetade;
+        if(sectionTop < 0) {
+            section.classList.add('ativo');
+        }
+    })
+}
+
+animaScroll();
+
+window.addEventListener('scroll', animaScroll);
+}
+}
+
+initAnimacaoScroll();
