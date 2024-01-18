2c60ccf7ab08ccddadb9d0d711fd4e18c88cdd37
Sarah Prando <sarahprando40@gmail.com>
2024-01-18 14:23:16 -0300

    ex js prototype

---
+++ javascript/exercicios/prototype.js
@@ -0,0 +1,44 @@
+// Crie uma função construtora de Pessoas
+// Deve conter nome, sobrenome e idade
+// Crie um método no protótipo que retorne
+// o nome completo da pessoa
+function Pessoa(nome, sobrenome, idade) {
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+    this.idade = idade;
+}
+
+const pessoa = new Pessoa('Sarah', 'Prando', 24);
+
+Pessoa.prototype.nomeCompleto = function() {
+    console.log (`${this.nome} ${this.sobrenome}`);
+  }
+
+pessoa.nomeCompleto();
+
+// Liste os métodos acessados por 
+// dados criados com NodeList,
+// HTMLCollection, Document
+
+Object.getOwnPropertyNames(NodeList.prototype);
+
+Object.getOwnPropertyNames(HTMLCollection.prototype);
+
+Object.getOwnPropertyNames(Document.prototype);
+
+// Liste os construtores dos dados abaixo
+
+const li = document.querySelector('li');
+
+li; // HTMLLIElement
+li.click; // Function
+li.innerText; // String
+li.value; // Number
+li.hidden; // Boolean
+li.offsetLeft; // Number
+li.click(); // Undefined
+
+// Qual o construtor do dado abaixo:
+li.hidden.constructor.name; // String
+
+
